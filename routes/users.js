b60e4bf1a15855fb753d8701f36653c8a0766e73
Gerardo <gerardoh13@gmail.com>
2023-01-07 01:20:18 -0800

    added random password further study

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,12 @@ const jsonschema = require("jsonschema");
 
 const express = require("express");
 const { ensureLoggedIn, ensureAdmin, ensureAdminOrSelf } = require("../middleware/auth");
-const { BadRequestError, NotFoundError } = require("../expressError");
+const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
 const userNewSchema = require("../schemas/userNew.json");
 const userUpdateSchema = require("../schemas/userUpdate.json");
+const generator = require('generate-password')
 
 const router = express.Router();
 
@@ -34,7 +35,10 @@ router.post("/", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
       const errs = validator.errors.map(e => e.stack);
       throw new BadRequestError(errs);
     }
-
+    req.body.password = generator.generate({
+      length: 10,
+      numbers: true
+    })
     const user = await User.register(req.body);
     const token = createToken(user);
     return res.status(201).json({ user, token });
